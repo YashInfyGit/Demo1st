c1d2458d712e4c4f0441a21994f9086fb989d18e
Sai computer <Sai computer@DESKTOP-NFA7VEO>
2022-01-31 21:15:31 +0530

    My 1st Git

---
+++ src/main/java/selenium_project/Selenium_Project.java
@@ -0,0 +1,10 @@
+package selenium_project;
+
+public class Selenium_Project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Bosssss!!!");
+	}
+
+}
